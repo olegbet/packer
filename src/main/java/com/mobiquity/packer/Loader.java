6c263f54ecb92238f479edd5ef152c1c0dd510db
olegbetsun <oleg.betsun@stratasys.com>
2021-05-18 11:17:38 +0300

    Packer: some cleanup

--- src/main/java/com/mobiquity/packer/Loader.java
+++ src/main/java/com/mobiquity/packer/Loader.java
@@ -43,22 +43,22 @@ public class Loader {
 		     {
 		    	 
 		    	 Integer index = Integer.valueOf(m.group("i"));
-	             Double weight = Double.valueOf(m.group("w"));
+		    	 Double weight = Double.valueOf(m.group("w"));
 	             
-	             if(weight.compareTo((double) Utils.MAX_ITEM_WEIGHT)>0)
+		    	 if(weight.compareTo((double) Utils.MAX_ITEM_WEIGHT)>0)
 		    		 throw new APIException("Item weight "+weight+" exceeds limit, max item weight is "+Utils.MAX_ITEM_WEIGHT);
 	             
-	             Double cost = Double.valueOf(m.group("c")); 
+		    	 Double cost = Double.valueOf(m.group("c")); 
 	             
-	             if(cost.compareTo((double) Utils.MAX_ITEM_COST)>0)
+		    	 if(cost.compareTo((double) Utils.MAX_ITEM_COST)>0)
 		    		 throw new APIException("Item cost "+cost+" exceeds limit, max item cost is "+Utils.MAX_ITEM_COST);
 		    	 
-	             Item item = new Item(index, weight, cost);
+		    	 Item item = new Item(index, weight, cost);
 	             
-	             pack.put(item);
+		    	 pack.put(item);
 	             
-	             if(pack.getContentSize() > Utils.MAX_ITEMS)
-	            	 throw new APIException("Item count "+pack.getContentSize()+" exceeds limit, max item count is "+Utils.MAX_ITEMS);
+		    	 if(pack.getContentSize() > Utils.MAX_ITEMS)
+		    		 throw new APIException("Item count "+pack.getContentSize()+" exceeds limit, max item count is "+Utils.MAX_ITEMS);
 		     }
 		     
 		     packages.add(pack);
